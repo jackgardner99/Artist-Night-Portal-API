3669bdc020e23d102a2f85390543f28e7ec4c224
Jack Gardner <jtgardner99@gmail.com>
2026-04-12 13:18:20 -0500

    fixtures made

--- aportalapi/models/user_utilities.py
+++ aportalapi/models/user_utilities.py
@@ -1,6 +1,5 @@
 from django.db import models
 from django.contrib.auth.models import User
-from .charts import Chart
 
 class UserUtilities(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE, related_name="user")
