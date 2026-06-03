bd3214c17efc46c192f4ed4aad52d8481d00c9c2
Jack Gardner <jtgardner99@gmail.com>
2026-06-03 12:14:13 -0500

    initial commit'

--- aportalproject/storage.py
+++ aportalproject/storage.py
@@ -1,14 +1,28 @@
+import cloudinary
 import cloudinary.uploader
+import cloudinary.utils
 from cloudinary_storage.storage import MediaCloudinaryStorage
+
+RAW_EXTENSIONS = {'pdf', 'docx', 'doc', 'txt', 'zip'}
+
+
+def get_resource_type(name):
+    ext = name.rsplit('.', 1)[-1].lower() if '.' in name else ''
+    return 'raw' if ext in RAW_EXTENSIONS else 'image'
 
 
 class AutoTypeCloudinaryStorage(MediaCloudinaryStorage):
     def _save(self, name, content):
+        resource_type = get_resource_type(name)
         response = cloudinary.uploader.upload(
             content,
             public_id=name,
-            resource_type='auto',
-            use_filename=True,
-            unique_filename=True,
+            resource_type=resource_type,
+            overwrite=True,
         )
         return response['public_id']
+
+    def url(self, name):
+        resource_type = get_resource_type(name)
+        url, _ = cloudinary.utils.cloudinary_url(name, resource_type=resource_type)
+        return url
